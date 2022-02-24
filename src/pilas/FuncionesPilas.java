c660d69fc8adeb8ce366082392f7dcd3a3dfea3d
AI-2003 <carlos.ibarraran@outlook.com>
2022-02-24 12:59:03 -0600

    Problema 17 completo, con metodo quitaRepetidos() como extra.

--- src/pilas/FuncionesPilas.java
+++ src/pilas/FuncionesPilas.java
@@ -32,4 +32,65 @@ public class FuncionesPilas {
         for(int i=0; i<elementos.size(); i++)
             pila.push(elementos.get(i));        
     }
+    
+    public static <T> void quitaRepetidos(PilaA<T> pila){
+        T[] arr;
+        T obj;
+        int cantObj, i;
+        
+        arr=(T[]) new Object[length(pila)];
+        cantObj=0;
+        while(!pila.isEmpty()){
+            obj=pila.pop();
+            i=0;
+            while(i<arr.length && !obj.equals(arr[i]))
+                i++;
+            if(i==arr.length){
+                arr[cantObj]=obj;
+                cantObj++;
+            }
+        }
+        for(int j=cantObj-1; j>0; j--)
+            pila.push(arr[j]);
+    }
+    
+    public static <T> void quitaRepetidosConsecutivos(PilaA<T> pila){
+        int cantObj, pilaLength;
+        T[] arr;
+        T obj;
+        
+        cantObj=0;
+        pilaLength=length(pila);
+        arr=(T[]) new Object[pilaLength];
+        for(int i=0; i<pilaLength-2; i++){
+            obj=pila.pop();
+            if(!obj.equals(pila.peek())){
+                arr[cantObj]=obj;
+                cantObj++;
+            }
+        }
+        for(int i=0; i<cantObj; i++)
+            pila.push(arr[i]);
+    }
+    
+    public static void main(String[] args){
+        PilaA p=new PilaA();
+        
+        p.push(1);
+        p.push(2);
+        p.push(3);
+        p.push(4);
+        p.push(1);
+        p.push(5);
+        p.push(6);
+        p.push(3);
+        p.push(7);
+        p.push(2);
+        
+        System.out.println(length(p));
+        quitaRepetidos(p);
+        System.out.println(length(p));
+        System.out.println(p.peek());
+        
+    }
 }
