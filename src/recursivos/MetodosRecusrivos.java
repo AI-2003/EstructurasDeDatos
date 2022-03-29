2f16683db5b840ab474e80dc1fc1adee9e485862
AI-2003 <carlos.ibarraran@outlook.com>
2022-03-29 12:53:50 -0600

    Métodos recursivos añadidos.

--- src/recursivos/MetodosRecusrivos.java
+++ src/recursivos/MetodosRecusrivos.java
@@ -64,19 +64,50 @@ public class MetodosRecusrivos {
         }
     }
     
+    public static double sumaPorRenglon(double[][] mat, int n, int ren){
+        
+        if(n-1<0)
+            return 0;
+        else
+            return mat[ren][n-1]+sumaPorRenglon(mat, n-1, ren);
+    }
+    
+    public static <T> String toString(T[][] mat, int m, int n){
+        
+        return toString(mat, 0, 0, m, n);
+    }
+    
+    private static <T> String toString(T[][] mat, int m, int n, int totR, int totC) {
+        if(m==totR)
+            return "";
+        else
+            if(n==totC)
+                return "\n"+toString(mat,m+1,0,totR,totC);
+            else
+                return mat[m][n]+toString(mat,m,n+1,totR,totC)+" ";
+    }
+    
+    public static int fib(int n){
+        int[] fibs;
+        
+        fibs=new int[n+1];
+        fibs[0]=0;
+        fibs[1]=1;
+        return fib(fibs, n);
+    }
+    
+    private static int fib(int[] fibs, int n){
+        if(fibs[n]==0&&n!=0)
+            return fib(fibs, n-1) + fib(fibs, n-2);
+        else
+            return fibs[n];
+    }
+    
     
     
     public static void main(String[] args){
-        System.out.println(calcFactorial(5));
-        System.out.println(calcFactorialMaestra(5));
+        Integer[][] mat = {{1,1,1,1,1},{1,2,3,4,5},{1,1,1,1,1}};
         
-        int[] arr={5,10,15,20,25};
-        System.out.println(sumaArreglo(arr, 5));
-        System.out.println(sumaArregloMaestra(arr, 5));
-        
-        imprimeElemDI(arr,5);
-        System.out.println();
-        imprimeElemID(arr,5);
-        System.out.println();
+        System.out.println(fib(8));
     }
 }
